3cae4c7f174b4a5b9c1acd213246dc242a39c340
Anit Kushwaha <kushwahaanitak@gmail.com>
2026-08-16 23:13:00 +0530

    feat: improve validation for user registration and login; enhance error handling

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -2,11 +2,17 @@ import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-// ------------------------------ REGISTER ------------------------------------- //
-
 export const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
+
+    // Required Fields Validations
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
 
     // Check Existing User
     const existingUser = await User.findOne({ email });
@@ -19,18 +25,15 @@ export const registerUser = async (req, res) => {
       });
     }
 
-    // Hash Password
     const hashedPassword = await bcrypt.hash(password, 10);
     console.log("Hashed: ", hashedPassword);
 
-    // For Creating User Document in MongoDB  ( SAVE USER )
     const user = await User.create({
       name,
       email,
       password: hashedPassword,
     });
 
-    // Response
     return res.status(201).json({
       success: true,
       message: "User Registered Successfully",
@@ -52,12 +55,21 @@ export const registerUser = async (req, res) => {
 
 // ------------------------------ LOGIN ------------------------------------- //
 
-export const loginUser = async (req, res, next) => {
+export const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
 
+    // Validation
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email and Password are Required",
+      });
+    }
+
     // Find User
     const user = await User.findOne({ email });
+
     if (!user) {
       return res.status(404).json({
         success: false,
@@ -67,6 +79,7 @@ export const loginUser = async (req, res, next) => {
 
     // Compare Password
     const isPasswordMatch = await bcrypt.compare(password, user.password);
+
     if (!isPasswordMatch) {
       return res.status(401).json({
         success: false,
@@ -74,13 +87,12 @@ export const loginUser = async (req, res, next) => {
       });
     }
 
-    // JWT Payload
+    // Password Match
     const payload = {
       id: user._id,
       email: user.email,
       role: user.role,
     };
-    6;
 
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
       expiresIn: "1d",
@@ -88,7 +100,7 @@ export const loginUser = async (req, res, next) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: false,
       sameSite: "lax",
       maxAge: 24 * 60 * 60 * 1000,
     });
@@ -96,21 +108,18 @@ export const loginUser = async (req, res, next) => {
     return res.status(200).json({
       success: true,
       message: "Login Successful",
-      data: {
-        user: {
-          id: user._id,
-          name: user.name,
-          email: user.email,
-          role: user.role,
-        },
-      },
     });
   } catch (error) {
-    next(error);
+    console.error(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
   }
 };
 
-// --------------------------------- Get Profile -------------------------------- //
+// ----------------------------- Get Profile -------------------------------- //
 
 export const getProfile = async (req, res) => {
   try {
@@ -138,7 +147,7 @@ export const getProfile = async (req, res) => {
   }
 };
 
-// ------------------------------ Update-Profile --------------------------- //
+// ---------------------- Update-Profile --------------------------- //
 
 export const updateProfile = async (req, res) => {
   try {
@@ -162,7 +171,7 @@ export const updateProfile = async (req, res) => {
     }
 
     // Update user
-    const updatedUser = await User.findByIdAndUpdate(
+    const updateUser = await User.findByIdAndUpdate(
       req.user.id,
       {
         name,
@@ -172,26 +181,9 @@ export const updateProfile = async (req, res) => {
         new: true,
         runValidators: true,
       },
-    ).select("-password");
-
-    if (!updatedUser) {
-      return res.status(404).json({
-        success: false,
-        message: "User not found",
-      });
-    }
-
-    return res.status(200).json({
-      success: true,
-      message: "Profile Updated Successfully",
-      data: updatedUser,
-    });
+    );
   } catch (error) {
-    return res.status(500).json({
-      success: false,
-      message: "Internal Server Error",
-      error: error.message,
-    });
+    console.error();
   }
 };
 
@@ -199,253 +191,11 @@ export const updateProfile = async (req, res) => {
 
 export const logoutUser = async (req, res) => {
   try {
-    res.clearCookie("token", {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
-    });
+    res.clearCookie("token");
 
     return res.status(200).json({
       success: true,
       message: "Logout Successful",
-    });
-  } catch (error) {
-    console.error(error);
-
-    return res.status(500).json({
-      success: false,
-      message: "Internal Server Error",
-    });
-  }
-};
-
-// ------------------------------ Change-Password ------------------------------------- //
-
-export const changePassword = async (req, res) => {
-  try {
-    const { currentPassword, newPassword } = req.body;
-
-    // 1. Validation
-    if (!currentPassword || !newPassword) {
-      return res.status(400).json({
-        success: false,
-        message: "Current password and new password are required",
-      });
-    }
-
-    // 2. Find logged-in user
-    const user = await User.findById(req.user.id);
-
-    if (!user) {
-      return res.status(404).json({
-        success: false,
-        message: "User not found",
-      });
-    }
-
-    // 3. Check current password
-    const isPasswordMatch = await bcrypt.compare(
-      currentPassword,
-      user.password,
-    );
-
-    if (!isPasswordMatch) {
-      return res.status(401).json({
-        success: false,
-        message: "Current password is incorrect",
-      });
-    }
-
-    // 4. Hash new password
-    const hashedPassword = await bcrypt.hash(newPassword, 10);
-
-    // 5. Update password
-    user.password = hashedPassword;
-
-    // 6. Save user
-    await user.save();
-
-    // 7. Response
-    return res.status(200).json({
-      success: true,
-      message: "Password changed successfully",
-    });
-  } catch (error) {
-    console.error(error);
-
-    return res.status(500).json({
-      success: false,
-      message: "Internal Server Error",
-    });
-  }
-};
-
-// ------------------------------ Delete-Account ------------------------------------- //
-
-export const deleteAccount = async (req, res) => {
-  try {
-    // Find logged-in user
-    const user = await User.findById(req.user.id);
-
-    if (!user) {
-      return res.status(404).json({
-        success: false,
-        message: "User not found",
-      });
-    }
-
-    // Delete user
-    await User.findByIdAndDelete(req.user.id);
-
-    // Clear JWT cookie
-    res.clearCookie("token", {
-      httpOnly: true,
-      secure: false,
-      sameSite: "lax",
-    });
-
-    return res.status(200).json({
-      success: true,
-      message: "Account deleted successfully",
-    });
-  } catch (error) {
-    console.error(error);
-
-    return res.status(500).json({
-      success: false,
-      message: "Internal Server Error",
-    });
-  }
-};
-
-// ------------------------------ Admin-Dashboard ------------------------------------- //
-
-export const adminDashboard = async (req, res) => {
-  return res.status(200).json({
-    success: true,
-    message: "Welcome to Admin Dashboard",
-    user: req.user,
-  });
-};
-
-// ------------------------------ Get-All-Users ------------------------------------- //
-
-export const getAllUsers = async (req, res) => {
-  try {
-    const users = await User.find().select("-password");
-
-    return res.status(200).json({
-      success: true,
-      message: "Users fetched successfully",
-      count: users.length,
-      data: users,
-    });
-  } catch (error) {
-    console.log(error);
-
-    return res.status(200).json({
-      success: false,
-      message: "Internal Server Error",
-    });
-  }
-};
-
-// ------------------------------ Delete-Users-By-Admin ------------------------------------- //
-
-export const deleteUserByAdmin = async (req, res) => {
-  try {
-    // Get user ID from URL
-    const userId = req.params.id;
-
-    // Check if user exists
-    const user = await User.findById(userId);
-
-    if (!user) {
-      return res.status(404).json({
-        success: false,
-        message: "User not found",
-      });
-    }
-
-    // Prevent admin from deleting himself
-    if (user._id.toString() === req.user.id) {
-      return res.status(400).json({
-        success: false,
-        message: "Admin cannot delete himself",
-      });
-    }
-
-    // Delete user
-    await User.findByIdAndDelete(userId);
-
-    return res.status(200).json({
-      success: true,
-      message: "User deleted successfully",
-    });
-  } catch (error) {
-    console.error(error);
-
-    return res.status(500).json({
-      success: false,
-      message: "Internal Server Error",
-    });
-  }
-};
-
-// ------------------------------ Update-User-Role ------------------------------------- //
-
-export const updateUserRole = async (req, res) => {
-  try {
-    const userId = req.params.id;
-    const { role } = req.body;
-
-    // Validate role
-    if (!role) {
-      return res.status(400).json({
-        success: false,
-        message: "Role is required",
-      });
-    }
-
-    if (!["user", "admin"].includes(role)) {
-      return res.status(400).json({
-        success: false,
-        message: "Invalid role",
-      });
-    }
-
-    // Find target user
-    const user = await User.findById(userId);
-
-    if (!user) {
-      return res.status(404).json({
-        success: false,
-        message: "User not found",
-      });
-    }
-
-    // Prevent admin from changing his own role
-    if (user._id.toString() === req.user.id) {
-      return res.status(400).json({
-        success: false,
-        message: "You cannot change your own role",
-      });
-    }
-
-    // Update role
-    user.role = role;
-
-    await user.save();
-
-    return res.status(200).json({
-      success: true,
-      message: "User role updated successfully",
-      data: {
-        id: user._id,
-        name: user.name,
-        email: user.email,
-        role: user.role,
-      },
     });
   } catch (error) {
     console.error(error);
