1762977b668fbe14233e17a4353dc4a4b53c25d4
Anit Kushwaha <kushwahaanitak@gmail.com>
2026-08-16 23:12:18 +0530

    refactor: remove unused middleware and clean up auth routes

--- server/server.js
+++ server/server.js
@@ -2,26 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./src/config/db.js";
 import authRouter from "./src/routes/auth.route.js";
-import errorMiddleware from "./src/middlewares/error.middleware.js";
-import notFoundMiddleware from "./src/middlewares/notFound.middleware.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
-
 connectDB();
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-
 app.use("/auth", authRouter);
-
-// 404 Middleware
-app.use(notFoundMiddleware);
-
-// Error Middleware
-app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 5000;
 
--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,45 +1,18 @@
 import express from "express";
-import authMiddleware from "../middlewares/auth.middleware.js";
-import adminMiddleware from "../middlewares/role.middleware.js";
-import {
-  registerValidator,
-  loginValidator,
-} from "../validators/auth.validators.js";
 import {
   loginUser,
   registerUser,
   getProfile,
   logoutUser,
-  updateProfile,
-  changePassword,
-  deleteAccount,
-  adminDashboard,
-  getAllUsers,
-  deleteUserByAdmin,
-  updateUserRole,
 } from "../controllers/auth.controller.js";
+
+import authMiddleware from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/register", registerValidator, registerUser);
-router.post("/login", loginValidator, loginUser);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 router.get("/profile", authMiddleware, getProfile);
-router.put("/profile", authMiddleware, updateProfile);
-router.patch("/change-password", authMiddleware, changePassword);
-router.delete("/account", authMiddleware, deleteAccount);
 router.post("/logout", logoutUser);
-router.get("/admin", authMiddleware, adminMiddleware, adminDashboard);
-router.get("/admin/users", authMiddleware, adminMiddleware, getAllUsers);
-router.delete(
-  "/admin/users/:id",
-  authMiddleware,
-  adminMiddleware,
-  deleteUserByAdmin,
-);
-router.patch(
-  "/admin/users/:id/role",
-  authMiddleware,
-  adminMiddleware,
-  updateUserRole,
-);
+
 export default router;
