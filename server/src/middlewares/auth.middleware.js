20dbdd3d05576ef613e20dfd128cd233a2fd9c71
Anit Kushwaha <kushwahaanitak@gmail.com>
2026-08-25 16:54:46 +0530

    feat: enhance auth middleware to verify user existence and account status; improve error handling

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,27 +1,44 @@
 import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
 
-const authMiddleware = (req, res, next) => {
+const authMiddleware = async (req, res, next) => {
   try {
-    const token = req.cookies.accessToken;                              // 1. get Cookie form the token
-    
-    if (!token) {                                                 // 2. Token exist ot not
+    const token = req.cookies.accessToken;
+
+    if (!token) {
       return res.status(401).json({
         success: false,
         message: "Authentication required",
       });
     }
-    
-    const decode = jwt.verify(token, process.env.JWT_SECRET);      // 3. Token Verify
-    
-    console.log("Decode JWT:", decode);
-    
-    req.user = decode;                                             // 4. Add request in User information
-    
-    next();                                                        // 5. Next middleware or Controller
-    
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.id).select("_id role isActive");
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    if (!user.isActive) {
+      return res.status(403).json({
+        success: false,
+        message: "Your account has been blocked",
+      });
+    }
+
+    req.user = {
+      id: user._id,
+      role: user.role,
+    };
+
+    next();
   } catch (error) {
     console.error(error);
-    
+
     return res.status(401).json({
       success: false,
       message: "Access token expired or invalid",
